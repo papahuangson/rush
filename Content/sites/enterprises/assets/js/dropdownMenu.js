f15fd421b8c50b52e94078fc2079aaebf2414326
mjohnson290 <michelangelo.johnson@gmail.com>
2019-11-19 10:36:06 -0600

    dropdown and mobile menu tweaks

---
+++ Content/sites/enterprises/assets/js/dropdownMenu.js
@@ -0,0 +1,32 @@
+// Dropdown Menu
+let menuIsOpen = false; // used for handling clicks on button
+function toggleMenu(event, menuId) {
+  event.preventDefault();
+
+  // if menu is open and there's a click - skip the open logic and close the menu
+  if (!menuIsOpen) {
+    const elem = document.getElementById(menuId);
+    const buttonElement = event.target;
+
+    document.getElementById(menuId).style.display = 'block';
+    buttonElement.setAttribute('aria-expanded', 'true');
+    menuIsOpen = true;
+
+    // add event listener for clicks outside element
+    const outsideClickListener = (event) => {
+      if (!elem.contains(event.target) && event.target.closest(`#${menuId}`) === null) {
+        elem.style.display = 'none';
+        buttonElement.setAttribute('aria-expanded', 'false');
+        menuIsOpen = false;
+        removeClickListener();
+      }
+    }
+
+    const removeClickListener = () => {
+      document.removeEventListener('click', outsideClickListener);
+    }
+
+    // add a setTimeout to allow bubbling to die down
+    setTimeout(() => document.addEventListener('click', outsideClickListener), 1);
+  }
+}
